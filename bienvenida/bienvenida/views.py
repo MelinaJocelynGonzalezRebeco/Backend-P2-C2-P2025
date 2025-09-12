93ebf5851ff9e8d911f1024226c8c33defc6cafa
MelinaJocelynGonzalezRebeco <145045907+MelinaJocelynGonzalezRebeco@users.noreply.github.com>
2025-09-12 15:46:54 -0300

    Mostrar bienvenida

--- bienvenida/bienvenida/views.py
+++ bienvenida/bienvenida/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from  django.shortcuts import render
+from . import models
 
 def inicio(request):
     return HttpResponse("Hola mundo desde Django")
+
+
+def mostrar_bienvenida(request):
+    tu_nombre = "Melina Gonzalez"
+    return HttpResponse(f"Bienvenidos a mi primera app Django, {tu_nombre}f")
+
+
+def lista_productos(request):
+    productos =  models.Producto.objects.all()
+    return render(request, productos/lista")
